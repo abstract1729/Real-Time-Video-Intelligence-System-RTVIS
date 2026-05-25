e186f30dca962f158f5f5f23a03a3a2dae74c73f
Arjun Mallick <arjunmallick99@gmail.com>
2026-05-25 13:17:09 +0530

    minor code changes

--- src/ingestion/stream_loader.py
+++ src/ingestion/stream_loader.py
@@ -16,4 +16,25 @@ StreamLoader
 VideoCaptureManager
 
 """
+from src.ingestion.video_capture import VideoCaptureManager
 
+class StreamLoader:
+    """
+    Responsible for loading the correct
+    video stream source implementation.
+    """
+
+    def __init__(self, ingestion_config: dict) -> None:
+        self.ingestion_config = ingestion_config
+
+    def load_stream(self) -> VideoCaptureManager:
+
+        source_type = self.ingestion_config.get("source_type")
+        source = self.ingestion_config.get("source")
+
+        capture_manager = VideoCaptureManager(
+            src=source,
+            src_type=source_type
+        )
+
+        return capture_manager
